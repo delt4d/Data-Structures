8b419443d681c231485661443f644fdb92c0a6e8
Leandro Cruz <LeandroCruz717@outlook.com>
2023-08-29 09:52:03 -0300

    update program.cd

--- Queue/C#/Program.cs
+++ Queue/C#/Program.cs
@@ -1,5 +1,18 @@
 ﻿using C_;
 
+void FindNode<T>(MyQueue<T> queue, T num)
+{
+    var result = queue.Query(num, out var currentNode, out var previousNode);
+
+    Console.WriteLine($"\nNumber {num} found? " + result);
+
+    if (result == true)
+    {
+        Console.WriteLine($"Previous node: {(previousNode != null ? previousNode.Value : "")}");
+        Console.WriteLine($"Current node: {currentNode!.Value}");
+    }
+}
+
 var queue = new MyQueue<int>();
 queue.Insert(12);
 queue.Insert(7);
@@ -7,12 +20,11 @@ queue.Insert(24);
 
 queue.Show();
 
-var num = 10;
-var result = queue.Query(7, out var currentNode, out var previousNode);
+FindNode(queue, 2);
+FindNode(queue, 24);
+FindNode(queue, 132);
+FindNode(queue, 7);
 
-Console.WriteLine($"\nNumber {num} found? " + result);
-Console.WriteLine($"Previous node: {previousNode?.Value}");
-Console.WriteLine($"Current node: {currentNode?.Value}");
 Console.WriteLine();
 
 queue.Remove();
