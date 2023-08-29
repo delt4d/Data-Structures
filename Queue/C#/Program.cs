04487c2705460119415edb08e36bb17613d0cfef
Leandro Cruz <LeandroCruz717@outlook.com>
2023-08-29 09:37:58 -0300

    feat: C# Queue implementation

---
+++ Queue/C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using C_;
+
+var queue = new MyQueue<int>();
+queue.Insert(12);
+queue.Insert(7);
+queue.Insert(24);
+
+queue.Show();
+
+var num = 10;
+var result = queue.Query(7, out var currentNode, out var previousNode);
+
+Console.WriteLine($"\nNumber {num} found? " + result);
+Console.WriteLine($"Previous node: {previousNode?.Value}");
+Console.WriteLine($"Current node: {currentNode?.Value}");
+Console.WriteLine();
+
+queue.Remove();
+queue.Remove();
+queue.Remove();
+
+queue.Show();
