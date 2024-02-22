8e231d56f52476ad51a3c751cf7b324e20d1f06a
Leandro Cruz <LeandroCruz717@outlook.com>
2024-02-22 15:19:47 -0300

    chore: delete folder called old

--- old/Queue/C++/build/CMakeFiles/C__.dir/src/Node.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/C__.dir/src/Node.cpp.obj: \
- C:\Users\Leandro\OneDrive\DELT4D\AULAS\DATA_STRUCTURE\Queue\C++\src\Node.cpp \
- C:\Users\Leandro\OneDrive\DELT4D\AULAS\DATA_STRUCTURE\Queue\C++\src\Node.h \
- C:\Users\Leandro\OneDrive\DELT4D\AULAS\DATA_STRUCTURE\Queue\C++\src\Node.cpp
